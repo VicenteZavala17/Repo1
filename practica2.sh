1a8aa2aaef3e4cff15b4c557fa5b88794448ca6a
VicenteZavala17 <vzavala16317@plataforma-utslp.net>
2024-03-02 01:43:00 -0500

    Edicion practica2 y suma.sh

--- practica2.sh
+++ practica2.sh
@@ -1,13 +1,13 @@
 #! /bin/bash
-file=cpu.log
-path=/var/log/
+nombre_archivo=cpu.log
+directorio=$PW/cpu.log
 
-cd $path
-if !(find $file)
+cd $directorio
+if !(find $nombre_archivo)
 then
-        touch $file
+        touch $nombre_archivo
 fi
 while true; do
-echo -e "$(date +%d/%m/%Y-%H:%M:%S)\t$(hostname -A)\t$(grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage}')\t$(free -m | awk '/Mem/{print int($3/$2*100)}') " >> $file
-      sleep 1
+echo -e "$(date +%d/%m/%Y-%H:%M:%S)\t$(hostname -A)\t$(grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage}')\t$(free -m | awk '/Mem/{print int($3/$2*100)}') " >> $nombre_archivo
+      sleep 2
 done
